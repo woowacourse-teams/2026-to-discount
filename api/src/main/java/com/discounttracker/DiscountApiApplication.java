5c64a477eae7d67ce2ce3321d8d65d341eb20120
nn98 <jkllhgb@gmail.com>
2026-07-27 16:59:00 +0900

    feat: 스프링 스캐폴드와 export.json 로더 추가

---
+++ api/src/main/java/com/discounttracker/DiscountApiApplication.java
@@ -0,0 +1,11 @@
+package com.discounttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountApiApplication.class, args);
+    }
+}
